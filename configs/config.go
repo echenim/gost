fe2c8e2deac53034910e764a5d2a7c477ee7ba39
echenim <myron.echenim@gmail.com>
2022-02-20 21:54:45 -0800

    config loader impleementation

--- configs/config.go
+++ configs/config.go
@@ -6,7 +6,7 @@ import (
 	"strings"
 )
 
-type Default struct{
+type DefaultCofig struct{
 	configData map[string]interface{}
 }
 
@@ -15,17 +15,17 @@ func Load(fileName string) (configz Configuration, err error){
     data, err = os.ReadFile(fileName)
 	if (err==nil){
 		decoder := json.NewDecoder(strings.NewReader(string(data)))
-		m := map[string]interface{} {}
+		m := map[string]interface{}{}
 		err = decoder.Decode(&m)
 		if (err == nil) {
-			configz = &Default{ configData: m }
+			configz = &DefaultCofig{configData: m}
 		}
 	}
 
 	return 
 }
 
-func (c *Default) get(name string) (result interface{}, found bool) {
+func (c *DefaultCofig) get(name string) (result interface{}, found bool) {
     data := c.configData
     for _, key := range strings.Split(name, ":") {
 		result, found = data[key]
@@ -38,13 +38,13 @@ func (c *Default) get(name string) (result interface{}, found bool) {
 	return
 }
 
-func (c *Default) GetString(name string) (result string, found bool){
+func (c *DefaultCofig) GetString(name string) (result string, found bool){
 	value, found := c.get(name)
     if (found) { result = value.(string) }
 	return 
 }
 
-func (c *Default) GetStringDefault(name, val string)(result string){
+func (c *DefaultCofig) GetStringDefault(name, val string)(result string){
 	result, ok := c.GetString(name)
     if !ok{
 		result = val
@@ -53,13 +53,13 @@ func (c *Default) GetStringDefault(name, val string)(result string){
 	return 
 }
 
-func (c *Default) GetInt(name string) (result int, found bool){
+func (c *DefaultCofig) GetInt(name string) (result int, found bool){
 	value, found := c.get(name)
     if (found) { result = value.(int) }
 	return 
 }
 
-func (c *Default) GetIntDefault(name string, val int)(result int){
+func (c *DefaultCofig) GetIntDefault(name string, val int)(result int){
 	result, ok := c.GetInt(name)
     if !ok{
 		result = val
@@ -68,13 +68,13 @@ func (c *Default) GetIntDefault(name string, val int)(result int){
 	return 
 }
 
-func (c *Default) GetBool(name string) (result bool, found bool){
+func (c *DefaultCofig) GetBool(name string) (result bool, found bool){
 	value, found := c.get(name)
     if (found) { result = value.(bool) }
 	return 
 }
 
-func (c *Default) GetBoolDefault(name string, val bool)(result bool){
+func (c *DefaultCofig) GetBoolDefault(name string, val bool)(result bool){
 	result, ok := c.GetBool(name)
     if !ok{
 		result = val
@@ -83,17 +83,29 @@ func (c *Default) GetBoolDefault(name string, val bool)(result bool){
 	return 
 }
 
-func (c *Default) GetFloat(name string)  (result float64, found bool) {
+func (c *DefaultCofig) GetFloat(name string)  (result float64, found bool) {
 	value, found := c.get(name)
     if (found) { result = value.(float64) }
 	return 
 }
 
-func (c *Default) GetFloatDefault(name string, val float64)(result float64){
+func (c *DefaultCofig) GetFloatDefault(name string, val float64)(result float64){
 	result, ok := c.GetFloat(name)
     if !ok{
 		result = val
 	}
 
+	return 
+}
+
+
+func (c *DefaultCofig) GetSection(name string)(section Configuration, found bool){
+	value,found := c.get(name)
+	if (found){
+		sectionData,ok:=value.(map[string]interface{})
+		if ok{
+			section = &DefaultCofig{configData: sectionData}
+		}
+	}
 	return 
 }
